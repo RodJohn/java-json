07eff111cd7d68f095628f546fbefd93e176bf49
John <rod_john@163.com>
2018-08-04 11:18:40 +0800

    init

--- src/main/java/com/john/java/tool/fastjson/JsonStringOnly.java
+++ src/main/java/com/john/java/tool/fastjson/JsonStringOnly.java
@@ -1,62 +1,62 @@
-//
-//package com.john.java.tool.fastjson;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import org.junit.Test;
-//
-//import com.alibaba.fastjson.JSONArray;
-//import com.alibaba.fastjson.JSONObject;
-//
-//public class JsonStringOnly {
-//
-//
-//	@Test
-//	//无需对象快速转化为json字符串
-//	public void toJSONString() throws Exception{
-//		JSONObject jsonParam = new JSONObject();
-//		jsonParam.put("website", 1);
-//		jsonParam.put("type", "衣服1");
-//		System.out.println("简单对象"+JSONObject.toJSON(jsonParam));
-//
-//		Map<String,Object> map = new HashMap<String,Object>();
-//		map.put("website", 2);
-//		map.put("type", "衣服2");
-//		JSONObject jsonParam1 = new JSONObject(map);
-//
-//		JSONArray array = new JSONArray();
-//		array.add(jsonParam);
-//		array.add(jsonParam1);
-//		System.out.println("数组对象"+JSONArray.toJSON(array));
-//
-//		Map<String,Object> map2 = new HashMap<String,Object>();
-//		map2.put("website", 3);
-//		map2.put("array", array);
-//		System.out.println("复杂对象"+JSONObject.toJSON(map2));
-//	}
-//
-//
-//
-//	@Test
-//	//json字符串	快速获取信息
-//	public void fromJSONString() throws Exception{
-//
-//		String json1 = "{\"website\":1,\"type\":\"衣服1\"}";
-//		JSONObject jsonParam1 = (JSONObject)JSONObject.parse(json1);
-//		System.out.println("简单JSON属性:"+jsonParam1.getString("type"));
-//
-//		String json2 = "[{\"website\":1,\"type\":\"衣服2\"},{\"website\":2,\"type\":\"衣服\"}]";
-//		JSONArray array = JSONObject.parseArray(json2);
-//		System.out.println("数组JSON属性:"+(array.getJSONObject(0)).getString("type"));
-//
-//
-//		String json3 = "{\"array\":[{\"website\":1,\"type\":\"衣服3\"},{\"website\":2,\"type\":\"衣服\"}],\"website\":3}";
-//		JSONObject jsonParam3 = (JSONObject)JSONObject.parse(json3);
-//		JSONArray jsonArray = jsonParam3.getJSONArray("array");
-//		System.out.println("复杂JSON属性:"+(jsonArray.getJSONObject(0)).getString("type"));
-//
-//	}
-//
-//
-//}
+
+package com.john.java.tool.fastjson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+public class JsonStringOnly {
+
+
+	@Test
+	//无需对象快速转化为json字符串
+	public void toJSONString() throws Exception{
+		JSONObject jsonParam = new JSONObject();
+		jsonParam.put("website", 1);
+		jsonParam.put("type", "衣服1");
+		System.out.println("简单对象"+JSONObject.toJSON(jsonParam));
+
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("website", 2);
+		map.put("type", "衣服2");
+		JSONObject jsonParam1 = new JSONObject(map);
+
+		JSONArray array = new JSONArray();
+		array.add(jsonParam);
+		array.add(jsonParam1);
+		System.out.println("数组对象"+JSONArray.toJSON(array));
+
+		Map<String,Object> map2 = new HashMap<String,Object>();
+		map2.put("website", 3);
+		map2.put("array", array);
+		System.out.println("复杂对象"+JSONObject.toJSON(map2));
+	}
+
+
+
+	@Test
+	//json字符串	快速获取信息
+	public void fromJSONString() throws Exception{
+
+		String json1 = "{\"website\":1,\"type\":\"衣服1\"}";
+		JSONObject jsonParam1 = (JSONObject)JSONObject.parse(json1);
+		System.out.println("简单JSON属性:"+jsonParam1.getString("type"));
+
+		String json2 = "[{\"website\":1,\"type\":\"衣服2\"},{\"website\":2,\"type\":\"衣服\"}]";
+		JSONArray array = JSONObject.parseArray(json2);
+		System.out.println("数组JSON属性:"+(array.getJSONObject(0)).getString("type"));
+
+
+		String json3 = "{\"array\":[{\"website\":1,\"type\":\"衣服3\"},{\"website\":2,\"type\":\"衣服\"}],\"website\":3}";
+		JSONObject jsonParam3 = (JSONObject)JSONObject.parse(json3);
+		JSONArray jsonArray = jsonParam3.getJSONArray("array");
+		System.out.println("复杂JSON属性:"+(jsonArray.getJSONObject(0)).getString("type"));
+
+	}
+
+
+}
--- src/main/java/com/john/java/tool/fastjson/Test1.java
+++ src/main/java/com/john/java/tool/fastjson/Test1.java
@@ -1,20 +1,21 @@
-//package com.john.java.tool.fastjson;
-//
-//
-//import com.john.java.tool.model.Person;
-//import org.junit.Test;
-//
-//import java.util.Date;
-//
-//public class T1ToString {
-//
-//    private Person person = new Person(1,"john",new Date());
-//
-//    @Test
-//    public void toString1(){
-//        System.out.println( JSONObject.toJSONString(person));
-//    }
-//
-//
-//
-//}
+package com.john.java.tool.fastjson;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.john.java.tool.model.Person;
+import org.junit.Test;
+
+import java.util.Date;
+
+public class Test1 {
+
+    private Person person = new Person(1,"john",new Date());
+
+    @Test
+    public void toString1(){
+        System.out.println( JSONObject.toJSONString(person));
+    }
+
+
+
+}
